fd8eeeb458f23458d6d2b30c422e8af864666757
Janniel Tan <tanjanniel@gmail.com>
2025-03-20 22:21:41 -0700

    Initial Commit

---
+++ TranslateMe/TranslateMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateMeApp.swift
+//  TranslateMe
+//
+//  Created by Janniel Tan on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct TranslateMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
